9afb4150f4d7b49f6bb3743026487931ab38a58f
Tung Nguyen <tongueroo@gmail.com>
2020-06-05 21:04:31 +0000

    update to random pet 2 words
    * ci build would sometimes fail due to non-unique storage account

--- lib/templates/hcl/stack/main.tf
+++ lib/templates/hcl/stack/main.tf
@@ -1,5 +1,6 @@
 resource "random_pet" "this" {
-  length = 1 # using 1, since default separator is '-', also account name can only be 24 characters, and lowercase letters
+  length = 2 # using 2, since default separator is '-', also account name can only be 24 characters, and lowercase letters
+  separator = ""# a blank string separator because azure storage accounts dont support - characters
 }
 
 resource "azurerm_resource_group" "this" {
