47dd488cca3713b4b03201094f0fe3ce5a17200b
Tung Nguyen <tongueroo@gmail.com>
2021-11-29 04:04:38 +0000

    change starter resource_group_name to have env

--- lib/templates/hcl/project/config/terraform/backend.tf
+++ lib/templates/hcl/project/config/terraform/backend.tf
@@ -2,7 +2,7 @@
 # This is useful because azure storage account names are not allowed special characters and are limited to 24 chars.
 terraform {
   backend "azurerm" {
-    resource_group_name  = "<%= expansion('terraform-resources-:LOCATION') %>"
+    resource_group_name  = "<%= expansion(':ENV-:LOCATION') %>"
     storage_account_name = "<%= expansion('ts:SUBSCRIPTION_HASH:LOCATION:ENV') %>"
     container_name       = "terraform-state"
     key                  = "<%= expansion(':LOCATION/:ENV/:BUILD_DIR/terraform.tfstate') %>"
